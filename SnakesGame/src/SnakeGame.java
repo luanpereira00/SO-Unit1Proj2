0008909a167b4fb79c6b8502db5c092edec9ff04
Luan <luanpereirast@gmail.com>
2017-09-02 20:35:11 -0300

    Snake's body creator conclued

--- SnakesGame/src/SnakeGame.java
+++ SnakesGame/src/SnakeGame.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.Dimension;
+
 class RunnableDemo implements Runnable {
    private Thread thread;
    private String threadName;
    
-   RunnableDemo( String name) {
+   RunnableDemo(String name) {
       threadName = name;
       System.out.println("Creating " +  threadName );
    }
@@ -31,9 +36,51 @@ class RunnableDemo implements Runnable {
 }
 
 public class SnakeGame {
+	private ArrayList<Snake> snakeList; //An ArrayList with all Snakes playable
+	public Screen board; //The board of the game
+	public int pxScale; //Pixel scale of rectangles of snake's body
+	
+	/**
+	 * Constructor for SnakeGame classe
+	 */
+	public SnakeGame(){
+		board = new Screen();
+		snakeList = new ArrayList<Snake>();
+		pxScale = 10;
+	}
+	
+	/**
+	 * Creates snake's body for the one that is being created 
+	 * @param start The start Dimension of the snake
+	 * @param end The end Dimension of the snake
+	 * @return Return a ArrayList with snake's body
+	 */
+	private ArrayList<Rectangle> createBody(Dimension start, Dimension end) {
+		ArrayList<Rectangle> body = new ArrayList<Rectangle>();
+		int xPos = (int) start.getWidth();
+		int yPos = (int) start.getHeight();
+		while(xPos < (int) end.getWidth()  && yPos == (int) end.getHeight()) {
+			body.add(new Rectangle(xPos, yPos, pxScale, pxScale));
+			xPos += pxScale;
+		}
+		return body;
+	}
+	
+	/**
+	 * Add a snake to snakeList's array
+	 * @param color The 
+	 * @param userOrIA
+	 */
+	public void addSnake(Color color, boolean userOrIA) {	
+		Dimension start = new Dimension(0,20);
+		Dimension end = new Dimension(40,20);
+		snakeList.add(new Snake(color, createBody(start, end), userOrIA));
+	}
+	
 	public static void main(String args[]) {
-		//Screen board = new Screen (); 
-	   	//board.addSnake(Snake(Color.blue, false));
+		SnakeGame game = new SnakeGame();
+		
+	   	game.addSnake(Color.yellow, false);
 	   	//while(True) {
 	   		//board.printSnakes();
 			//board.movementSnakes();
